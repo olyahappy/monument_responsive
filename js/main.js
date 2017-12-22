670be3be34711ae49f8fd4c45f00bd61a2457993
Olga <vasyndra18@mail.ru>
2017-12-22 11:55:04 -0800

    Revert "responsive website tutorial"
    This reverts commit 29281f3b88e5f30650414194c29e5149617a54e9.

--- js/main.js
+++
@@ -1,6 +0,0 @@
-//when you click on hamburger icon(.hamburger)
-$('.hamburger').on('click', function (event) {
-	// slide toggle the menu down (#dropdown)//
-	event.preventDefault();
-	$("#dropdown").toggle();
-});
